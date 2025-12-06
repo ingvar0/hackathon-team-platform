8d1f4ddd4c801f6214a5fcda582574809903a23b
Igor <igor0876321@gmail.com>
2025-12-06 14:01:34 +0300

    Revert "Настройка докерфайлов для фронта"

--- backend/api/admin/schemas.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class AdminLogin(BaseModel):
-    email: str
-    password: str
--- frontend/src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-
-
-function App() {
-  return (
-    <p>mfansd</p>
-  )
-}
-
-export default App
